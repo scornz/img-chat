569633a31a21af6cfb12bb24b45a0f991c9418b7
Mike Scornavacca <52425912+scornz@users.noreply.github.com>
2023-10-24 10:39:27 -0400

    Setup basic route structure of backend
    Taken from past project, tell-no-lie, has very similar use for OpenAI

---
+++ backend/api/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask, url_for
+from flask_cors import CORS
+from api.routes import register_routes
+
+# Modules
+import api.openai
+
+
+# Create Flask app in application factory format
+def create_app() -> Flask:
+    # Create and configure the app
+    app = Flask(__name__)
+
+    # Only allow requests from the specified domain
+    CORS(app)
+
+    # Register all exceptions for routes
+    from utils.exceptions import handle_exception
+
+    app.register_error_handler(Exception, handle_exception)
+
+    # # Register all routes with Flask app
+    register_routes(app)
+
+    return app
---
+++ backend/api/routes/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask, Blueprint
+
+
+# This will register all routes
+def register_routes(app: Flask):
+    from .root import root
+
+    # Register all API routes under this blueprint
+    api = Blueprint("api", __name__)
+
+    app.register_blueprint(root)
+    app.register_blueprint(api)
