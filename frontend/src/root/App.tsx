135fa8409a8e4b7354b39583e556b067a56389ba
Pierce Maloney <piercemaloney@dynamic-oit-vapornet100-10-9-77-134.princeton.edu>
2023-11-07 16:12:40 -0500

    connecting tabs thru stack

--- frontend/src/root/App.tsx
+++ frontend/src/root/App.tsx
@@ -1,51 +1,49 @@
-import {
-  createHashRouter,
-  Navigate,
-  Outlet,
-  RouterProvider,
-} from "react-router-dom";
-
 import { RecoilRoot } from "recoil";
 import RecoilNexus from "recoil-nexus";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import {
+  ChakraProvider,
+  Flex,
+  Tabs,
+  TabList,
+  Box,
+  Tab,
+} from "@chakra-ui/react";
 
-import { ChakraProvider } from "@chakra-ui/react";
-
-// Pages
 import Chat from "pages/Chat";
 import Prompt from "pages/Prompt";
 
-/* We use a hash router to take advantage of GitHub pages. GitHub pages uses
-server-side routing, so we need this. */
-const router = createHashRouter([
-  {
-    element: (
-      <ChakraProvider>
-        <Outlet />
-      </ChakraProvider>
-    ),
-    children: [
-      {
-        path: "/chat",
-        element: <Chat />,
-      },
-      {
-        path: "/prompt",
-        element: <Prompt />,
-      },
-    ],
-    errorElement: <Navigate to="/chat" />,
-  },
-]);
-
-/**
- * Root component for the app. Organizes navigation and initializes global
- * state (using Recoil/Nexus).
- */
 function App() {
   return (
     <RecoilRoot>
       <RecoilNexus />
-      <RouterProvider router={router} />
+      <Router>
+        <ChakraProvider>
+          <Flex direction="column" h="100vh" pt="20px">
+            <Tabs variant="soft-rounded" colorScheme="green">
+              <TabList justifyContent="center">
+                <Tab as={Link} to="/chat">
+                  Conversational
+                </Tab>
+                <Tab as={Link} to="/prompt">
+                  Single Prompt
+                </Tab>
+                <Tab as={Link} to="/help">
+                  Help
+                </Tab>
+              </TabList>
+            </Tabs>
+
+            <Box flex="1" width="100%">
+              <Routes>
+                <Route path="/chat" element={<Chat />} />
+                <Route path="/prompt" element={<Prompt />} />
+                <Route path="/help" element={<p>Help page</p>} />
+              </Routes>
+            </Box>
+          </Flex>
+        </ChakraProvider>
+      </Router>
     </RecoilRoot>
   );
 }
