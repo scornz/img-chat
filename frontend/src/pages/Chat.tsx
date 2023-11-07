8de548d4dd8710ba76a1f9df8c1200ebed1bba50
Henry Knoll <hknoll@princeton.edu>
2023-11-07 01:59:17 -0500

    Added Tabs to UI for switching between modes

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack } from "@chakra-ui/react";
+import { Box, Stack, Tabs, TabList, TabPanels, Tab, TabPanel, useColorModeValue } from "@chakra-ui/react";
 import MessageList from "containers/MessageList";
 import { useState } from "react";
 import { MessageInfo, MessageType, SenderType } from "components/Message";
@@ -73,25 +73,66 @@ function Chat() {
     setMessages((prev) => [info, ...prev]);
   };
 
+  const bg_colors = useColorModeValue(
+    ['teal.50', 'orange.50', 'purple.50'],
+    ['teal.900', 'orange.900', 'purple.900'],
+  );
+  const [tabIndex, setTabIndex] = useState(0);
+  const bg = bg_colors[tabIndex];
+
   return (
-    <Stack
-      maxWidth="750px"
-      maxHeight="100vh"
-      mx="auto"
-      h="full"
-      flexDirection="column"
-    >
-      <MessageList
-        width="full"
-        mt="auto"
-        justifyContent="flex-start"
-        overflow="scroll"
-        messages={messages}
-      />
-      <Box width="full">
-        <ChatInput mb="4" onSend={sendMessage} />
-      </Box>
-    </Stack>
+      <Tabs 
+        variant='soft-rounded' 
+        colorScheme='green'
+        //onChange={(index) => setTabIndex(index)} bg={bg}
+        h="100%"
+        mt="1vh"
+        overflow="hidden"
+      >
+        <TabList 
+          justifyContent="center"
+        >
+          <Tab>Conversational</Tab>
+          <Tab>Single Prompt</Tab>
+          <Tab>Help</Tab>
+        </TabList>
+
+        <TabPanels
+          h="95vh"
+        >
+          <TabPanel
+            height="100%"
+            >
+
+            <Stack
+              maxWidth="750px"
+              maxHeight="100%"
+              mx="auto"
+              h="full"
+              flexDirection="column"
+              flex="1"
+            >
+              <MessageList
+                width="full"
+                mt="auto"
+                justifyContent="flex-start"
+                overflow="scroll"
+                messages={messages}
+              />
+              <Box width="full">
+                <ChatInput mb="4" onSend={sendMessage} />
+              </Box>
+            </Stack>
+
+          </TabPanel>
+          <TabPanel>
+            <p>Single prompt page</p>
+          </TabPanel>
+          <TabPanel>
+            <p>Help page</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
   );
 }
 
